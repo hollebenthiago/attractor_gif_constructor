05655a6b4bf27350861b2b39a10cc66cdc7d54c7
thiago <hollebenthiago@gmail.com>
2020-05-25 01:03:46 -0300

    some more tests

--- function.py
+++ function.py
@@ -9,27 +9,27 @@ def F(t, xs, args):
   x = xs[0]                  
   y = xs[1]                  
   z = xs[2]                  
-  X = args[0]*(y-x)                     
-  Y = x*(args[1]-z)-y                     
-  Z = x*y-args[2]*z                     
+  X = np.sin(6*y) -np.cos(-0.3*z)                     
+  Y = -np.sin(3*x) + np.cos(16*z)                     
+  Z = np.cos(2*x) - np.sin(3*y)                     
   return np.array([X, Y, Z]) 
 h = 0.01   
-tmax = 100 
-args = [10, 28, 8/3]  
-x0 = np.array([8, 5, 13])    
-n = 2     
-Xs_m1 = euler(F, np.linspace(0, tmax, 1000), x0, h, args)[0] 
-xs_m1 = euler(F, np.linspace(0, tmax, 1000), x0, h, args)[0][0] 
-ys_m1 = euler(F, np.linspace(0, tmax, 1000), x0, h, args)[0][1] 
-zs_m1 = euler(F, np.linspace(0, tmax, 1000), x0, h, args)[0][2] 
-Xs_m2 = rk4(F, np.linspace(0, tmax, 1000), x0, h, args)[0] 
-xs_m2 = rk4(F, np.linspace(0, tmax, 1000), x0, h, args)[0][0] 
-ys_m2 = rk4(F, np.linspace(0, tmax, 1000), x0, h, args)[0][1] 
-zs_m2 = rk4(F, np.linspace(0, tmax, 1000), x0, h, args)[0][2] 
-Xs_m3 = modn(F, np.linspace(0, tmax, 1000), x0, h, n, eulerstep, rk4step, args)[0]    
-xs_m3 = modn(F, np.linspace(0, tmax, 1000), x0, h, n, eulerstep, rk4step, args)[0][0] 
-ys_m3 = modn(F, np.linspace(0, tmax, 1000), x0, h, n, eulerstep, rk4step, args)[0][1] 
-zs_m3 = modn(F, np.linspace(0, tmax, 1000), x0, h, n, eulerstep, rk4step, args)[0][2] 
+tmax = 2004  
+args = []  
+x0 = np.array([0.5, 0.5, 0.5])    
+n = 3     
+Xs_m1 = euler(F, np.linspace(0, tmax, tmax), x0, h, args)[0] 
+xs_m1 = euler(F, np.linspace(0, tmax, tmax), x0, h, args)[0][0] 
+ys_m1 = euler(F, np.linspace(0, tmax, tmax), x0, h, args)[0][1] 
+zs_m1 = euler(F, np.linspace(0, tmax, tmax), x0, h, args)[0][2] 
+Xs_m2 = rk4(F, np.linspace(0, tmax, tmax), x0, h, args)[0] 
+xs_m2 = rk4(F, np.linspace(0, tmax, tmax), x0, h, args)[0][0] 
+ys_m2 = rk4(F, np.linspace(0, tmax, tmax), x0, h, args)[0][1] 
+zs_m2 = rk4(F, np.linspace(0, tmax, tmax), x0, h, args)[0][2] 
+Xs_m3 = modn(F, np.linspace(0, tmax, tmax), x0, h, n, eulerstep, rk4step, args)[0]    
+xs_m3 = modn(F, np.linspace(0, tmax, tmax), x0, h, n, eulerstep, rk4step, args)[0][0] 
+ys_m3 = modn(F, np.linspace(0, tmax, tmax), x0, h, n, eulerstep, rk4step, args)[0][1] 
+zs_m3 = modn(F, np.linspace(0, tmax, tmax), x0, h, n, eulerstep, rk4step, args)[0][2] 
 fig    = plt.figure()                                    
 ax     = p3.Axes3D(fig)                                  
 labels = [euler.__name__, rk4.__name__, 'alternating']                       
@@ -42,13 +42,13 @@ data += [Xs_m1, Xs_m2, Xs_m3]
 ax.set_xlim3d([min(min(Xs_m1[0]),min(Xs_m2[0])), max(max(Xs_m1[0]),max(Xs_m2[0]))]) 
 ax.set_xlabel('X') 
 ax.set_ylim3d([min(min(Xs_m1[1]),min(Xs_m2[1])), max(max(Xs_m1[1]),max(Xs_m2[1]))]) 
-ax.set_xlabel('Y') 
+ax.set_ylabel('Y') 
 ax.set_zlim3d([min(min(Xs_m1[2]),min(Xs_m2[2])), max(max(Xs_m1[2]),max(Xs_m2[2]))]) 
-ax.set_xlabel('Z') 
+ax.set_zlabel('Z') 
 ax.grid(False) 
 ax.set_xticks([]) 
 ax.set_yticks([]) 
 ax.set_zticks([]) 
 ax.legend(loc='upper right') 
-line_ani = animation.FuncAnimation(fig, update_lines, 1000, fargs=(data, lines, ax),interval=50, blit=False) 
-line_ani.save('gifs/lorenz.mp4')
+line_ani = animation.FuncAnimation(fig, update_lines, np.arange(0, tmax, 2), fargs=(data, lines, ax),interval=50, blit=False) 
+line_ani.save('gifs/clifford3.mp4')
